95f9892c88833d231f8389ee86d6f658986e9241
anastasiaasadcheva94 <asadchevaanastasia@gmail.com>
2021-03-14 23:43:27 +0300

    init: task Reverse Int complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0){
+        var number = n * -1;
+        return Number(number.toString().split('').reverse().join(''));
+    } else {
+        return Number(n.toString().split('').reverse().join(''));
+    }
 }
